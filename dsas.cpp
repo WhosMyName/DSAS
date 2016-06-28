02311ec1fda6c9edb58db34de075cb38557d3b38
WhosMyName <WhosMyName@users.noreply.github.com>
2016-06-28 08:25:15 +0200

    ???

--- dsas.cpp
+++ dsas.cpp
@@ -1,16 +1,3 @@
-/*
-Dark Sous Auto Save - DSASv0.5.2 Hotfix 2
-
-Implement Auto Name Search
-Implement Time Retrieval
-Implement Looping Backup
-Implement Copying
-Implement Renaming
-Implement Exiting
-
-All Done!
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -33,11 +20,11 @@ using namespace boost::posix_time;
 using namespace boost;
 
 
-path OriginalPath = current_path(); //Gets The Current Path, which contains the Program
-path FinalPath; //Path where the Savefile should be
-path MainDir; //Path where all Saves are Stored
-path Year;
-path Month;
+boost::filesystem::path OriginalPath = current_path(); //Gets The Current Path, which contains the Program
+boost::filesystem::path FinalPath; //Path where the Savefile should be
+boost::filesystem::path MainDir; //Path where all Saves are Stored aka DarkSoulsAutoSave
+boost::filesystem::path Year; // DarkSoulsAutoSave\\2016
+boost::filesystem::path Month; // DarkSoulsAutoSave\\2016\\May
 
 string year;
 string month;
@@ -46,42 +33,40 @@ string dsname = "DRAKS0005.sl2";
 
 int mint; //Minutes to compare
 int premint; //Minutes to compare
-int maxlimit = 0;
+int maxlimit = 0; //Variable to prevent Infinite Loop, Max Value = 100
 
-typedef vector<path> vec; // store paths, so we can store and sort them later
+typedef vector<boost::filesystem::path> vec; // store paths, so we can store and sort them later
 vec v; //Vector for Source-File
 vec w; //Vector for Destination-Directory and Sorting
 
 
 
-void getDir(path p){
-	path temp = p;
-	path pretemp;
-	string stemp = temp.filename().string();
-	string docs = "Users";
+void getDir(boost::filesystem::path p){ //Basically trying to find out where to search for the Saves
+	boost::filesystem::path temp = p; //PreVariable to Cycle Back the Path
+	boost::filesystem::path pretemp; //PrePreVariable to Cycle Back the Path, should contain the User Folder
+	string stemp = temp.filename().string(); //Casting Path to String for Comparison
+	string docs = "Users"; //String
 	cout << "docs equals: " << docs << endl << endl; //Optional
 	cout << "stemp equals: " << stemp << endl << endl; //Optional
 	cout << "temp equals: " << temp.string() << endl << endl; //Optional
-	while(stemp.compare(docs) != 0){
-		pretemp = temp;
+	while(stemp.compare(docs) != 0){ //Reverse Clycling through Path
+		pretemp = temp; //Assignment of Paths
 		cout << "pretemp equals: " << pretemp.string() << endl << endl; //Optional
-		temp = temp.parent_path();
+		temp = temp.parent_path(); //Going up one Level in Path
 		cout << "temp equals: " << temp.string() << endl << endl; //Optional
-		stemp = temp.filename().string();
+		stemp = temp.filename().string(); //Casting Path to String for Comparison - Recast
 		cout << "stemp equals: " << stemp << endl << endl; //Optional
 	}
 	cout << "It's done! " << endl << endl; //Optional
-	string constructpath = pretemp.string();
+	string constructpath = pretemp.string(); //Combined with suffix to create FinalPath
 	cout << "pretemp finished equals: " << pretemp.string() << endl << endl; //Optional
-	string suffix = "\\Documents\\NBGI\\DarkSouls\\";
+	string suffix = "\\Documents\\NBGI\\DarkSouls\\"; //Path to DS Savefile
 	string Conv(constructpath + suffix); //Converted String Equals renamed Folders
-	path Dir;
-	Dir = Conv; // Cast String to Path
-	FinalPath = Dir;
-	cout << FinalPath.string() << " is the estimated Name of the Directory!" << endl << endl;
+ 	FinalPath = Conv; // Cast String to Path
+	cout << FinalPath.string() << " is the estimated Name of the Directory!" << endl << endl; //Optional
 }
 
-void makemain(){
+void makemain(){ //Creating The MainDirectory
 	string PreMain(FinalPath.string() + "DarkSoulsAutoSave\\");
 	MainDir = PreMain;
 	if (create_directory(MainDir)) { //Checks if creation of Directories Succeeds
@@ -101,35 +86,35 @@ ss2.str(string()); //Clears the Stringstream
 maxi.str(string()); //Clears the Stringstream
 mini.str(string()); //Clears the Stringstream
 
-time_facet *facet = new time_facet("%d_%b_%Y %H-%M-%S");
-ss.imbue(locale(ss.getloc(), facet));
-ss << second_clock::local_time();
-timestamp = ss.str();
+time_facet *facet = new time_facet("%d_%b_%Y %H-%M-%S"); //Creates a Timefacet with the Pattern "Day_Mon_Year Hour-Minutes-Seconds" 
+ss.imbue(locale(ss.getloc(), facet)); //Maps the Facet onto the Stringstream to match the Pattern
+ss << second_clock::local_time(); //Gets the Time and assigns it to the Stringstream
+timestamp = ss.str(); //Casts Stringstream to String
 ss.str(string()); //Clears the Stringstream
 
-time_facet *facetyear = new time_facet("%Y");
-ss1.imbue(locale(ss1.getloc(), facetyear));
-ss1 << second_clock::local_time();
-year = ss1.str();
+time_facet *facetyear = new time_facet("%Y"); //Creates a Timefacet with the Pattern "Year"
+ss1.imbue(locale(ss1.getloc(), facetyear)); //Maps the Facet onto the Stringstream to match the Pattern
+ss1 << second_clock::local_time(); //Gets the Time and assigns it to the Stringstream
+year = ss1.str(); //Casts Stringstream to String
 ss1.str(string()); //Clears the Stringstream
 
-time_facet *facetmonth = new time_facet("%B");
-ss2.imbue(locale(ss2.getloc(), facetmonth));
-ss2 << second_clock::local_time();
-month = ss2.str();
+time_facet *facetmonth = new time_facet("%B");//Creates a Timefacet with the Pattern "Month"
+ss2.imbue(locale(ss2.getloc(), facetmonth)); //Maps the Facet onto the Stringstream to match the Pattern
+ss2 << second_clock::local_time(); //Gets the Time and assigns it to the Stringstream
+month = ss2.str(); //Casts Stringstream to String
 ss2.str(string()); //Clears the Stringstream
 
-cout << "Year: " << year << " Month: " << month << endl;
+cout << "Year: " << year << " Month: " << month << endl; //Optional
 
-maxi << MainDir.string() << year << "\\";
+maxi << MainDir.string() << year << "\\"; //Adds Year to MainDir Path
 Year = maxi.str();
 maxi.str(string()); //Clears the Stringstream
-mini << Year.string() << month << "\\";
+mini << Year.string() << month << "\\"; //Adds Month to Year Path
 Month = mini.str();
 mini.str(string()); //Clears the Stringstream
 
-cout << "Year equals: " << Year.string() << endl << endl;
-cout << "Month equals: " << Month.string() << endl << endl;
+cout << "Year equals: " << Year.string() << endl << endl; //Optional
+cout << "Month equals: " << Month.string() << endl << endl; //Optional
 
 }
 
@@ -147,7 +132,7 @@ void showvecs(){ //Optional
 
 //................................................................................................\\
 
-void clearvecs(){
+void clearvecs(){ //Clears the Vectors to prevent File Duplications, or whatever the Bug I encountered was (hopefully)
 
 	v.erase(v.begin(), v.end());
 	w.erase(w.begin(), w.end());
@@ -161,11 +146,11 @@ void hitandrun(){
 
 	for (vec::iterator it (v.begin()); it != v.end(); ++it){		
 
-		path Sourcefile = (*it);
+		boost::filesystem::path Sourcefile = (*it);
 		stringstream ss3;
 		ss3.str(string());
 		ss3 << Month.string() << Sourcefile.filename().string();
-		path Destinationfile = ss3.str();
+		boost::filesystem::path Destinationfile = ss3.str();
 		cout << Destinationfile << endl; //Optional
 		copy_file(Sourcefile, Destinationfile);
 		cout << "Finished Copy Process!" << endl; //Optional
@@ -178,7 +163,7 @@ void hitandrun(){
 		stringstream pre;
 		pre << renamed.str().substr(0, found) << " - " << timestamp << renamed.str().substr(found);
 
-		path Renomnom = pre.str();
+		boost::filesystem::path Renomnom = pre.str();
 		cout << Renomnom.string() << endl << endl;
 		rename(Destinationfile, Renomnom);
 	}
@@ -189,7 +174,7 @@ void hitandrun(){
 }
 
 void timegate(){// gets the minutes and uses them for comparison to sync the saves in a X minute cycle
-	stringstream minutes;
+	std::stringstream minutes;
 	time_facet *facetminutes = new time_facet("%M");
 	minutes.imbue(locale(minutes.getloc(), facetminutes));
 	minutes << second_clock::local_time();
@@ -198,9 +183,9 @@ void timegate(){// gets the minutes and uses them for comparison to sync the sav
 
 }
 
-void checkexist(path p);
+void checkexist(boost::filesystem::path p);
 
-void createstorage(path p);
+void createstorage(boost::filesystem::path p);
 
 //##################################################################################################################################################################//
 int main(){
@@ -261,7 +246,7 @@ int main(){
 
 
 
-void checkexist(path p){
+void checkexist(boost::filesystem::path p){
 	try{
 		if (exists(p)) {  // does p actually exist?
 			if (is_regular_file(p)){  // is p a regular file?
@@ -297,7 +282,7 @@ void checkexist(path p){
 			}
 		}
 		else{
-			cout << p << " does not exist\n";
+			cout << p << " does not exist" << std::endl;
 		}
 	}
 	catch (const filesystem_error& ex){
@@ -305,7 +290,7 @@ void checkexist(path p){
 	}
 }
 
-void createstorage(path p){
+void createstorage(boost::filesystem::path p){
 	try{
 		if (exists(p)) {  // does p actually exist?
 			if (is_regular_file(p)){  // is p a regular file?
@@ -324,13 +309,12 @@ void createstorage(path p){
 						it2--;
 					}
 				}
-
 				for (vec::iterator it2 (w.begin()); it2 != w.end(); ++it2){ //Iteration cycles through Vector W
 					cout << (*it2).filename() << " was the latest added File!" << endl << endl;
 				}
 			}
 			else{
-				cout << p << " exists, but is neither a regular file nor a directory\n";
+				cout << p << " exists, but is neither a regular file nor a directory" << std::endl;
 			}
 		}
 		else{
@@ -343,3 +327,47 @@ void createstorage(path p){
 		cout << ex.what() << endl;
 	}
 }
+/*-----------------------------------------------SYMLINK-MAGIC-------------------------------------//
+void symlinking(boost::filesystem::path sym){
+	try{
+		if (exists(sym)) {  // does sym actually exist?
+			if (is_regular_file(sym)){  // is sym a regular file?
+        		cout << sym << " size is " << file_size(sym) << endl; //if it is output the filesize
+			}
+			else if (is_symlink(sym)){      // is sym a directory?
+				cout << sym << " is a Symlink !" << endl;
+
+			}
+			else{
+				cout << sym << " exists, but is neither a regular file nor a directory" << std::endl;
+			}
+		}
+		else{
+			cout << p << " does not exist" << std::endl;
+		}
+	}
+	catch (const filesystem_error& ex){
+		cout << ex.what() << endl;
+	}
+}
+--------------------------------------------------------------------------------------------------*///
+
+/*
+<-------------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!---------------------------------->
+COMMENT DSAS and MC2V!!! Holy.
+<-------------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!---------------------------------->
+Check if CurrentDir is in Roaming
+Yes:Skip
+Else:Create Symlink for DSII
+Let User Move DSASII into preferred	SteamID Folder
+Wait for any Input to ensure the Messag is read!!!!
+--------------------
+Check if DSASII is inside SteamID Folder
+Yes:Skip
+Else: Create Symlink for DSASII and SteamID Folder on Desktop inside DSASII
+--------------------
+Normal DSAS-Routine
+???
+Profit
+Praise!
+*/
